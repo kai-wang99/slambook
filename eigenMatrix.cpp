20014b7f43f50612369504b42d670d8635505208
kai-wang99 <k_wang99@hotmail.com>
2021-04-17 02:23:01 +0800

    init commit

---
+++ eigenMatrix.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
+int main( int argc, char** argv )
+{
+    Eigen::Matrix<float, 2, 3> matrix_23;
+    Eigen::Vector3d v_3d;
+    Eigen::Matrix<float,3,1> vd_3d;
+    Eigen::Matrix3d matrix_33 = Eigen::Matrix3d::Ones();
+    Eigen::MatrixXd matrix_x;
+
+    matrix_23 << 1, 2, 3, 4, 5, 6;
+    cout << matrix_23 << endl;
+
+    v_3d << 3, 2, 1;
+    vd_3d << 4,5,6;
+
+    Eigen::Matrix<double, 2, 1> result = matrix_23.cast<double>() * v_3d;
+    cout << result << endl;
+
+    Eigen::Matrix<float, 2, 1> result2 = matrix_23 * vd_3d;
+    cout << result2 << endl;
+
+    cout <<"hello world"  << endl;
+
+    return 0;
+}
